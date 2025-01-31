7ca87eab8bbd19c6472fa9e300e16da84913d038
Roman Zaitsev <66490718+Tefix@users.noreply.github.com>
2025-01-31 16:35:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("write ur name");
+            string nimi = Console.ReadLine();
+            Console.WriteLine("hello " + nimi);
+
+            if (string.IsNullOrEmpty(nimi) || !nimi.All(char.IsLetter))
+            {
+                Console.WriteLine("error");
+                Console.Beep();
+            }
+        }
+    }
+}
